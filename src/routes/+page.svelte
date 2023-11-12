baabadb860aa37356b81feee592f9a9db671ad99
Alp Kaan Aksu <hi@alpkaanaksu.com>
2023-11-12 17:47:46 +0100

    initial commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -23,11 +23,10 @@
 	}
 
 	function setup() {
-        const agent = new Agent({x: 0, y: 0});
-		const setupCode = '(environment) => {' + code + '\n}';
+		const setupCode = '(environment, Agent) => {' + code + '\n}';
 		const setupFunction = eval(setupCode);
 
-		setupFunction(environment);
+		setupFunction(environment, Agent);
 		ticks++;
 	}
 
