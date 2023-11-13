34f6243c9c4d03080ffee2fd939c37cc468087d3
Alp Kaan Aksu <hi@alpkaanaksu.com>
2023-11-13 18:30:36 +0100

    Create Herr Erwin code

--- src/defaults.js
+++ src/defaults.js
@@ -11,9 +11,98 @@ class Sheep extends Agent {
 
     tick() {
         this.orientation = exposed.orientation;
-        this.forward(exposed.speed);
+        this.go(exposed.speed);
     }
 }
 
 environment.add(new Sheep(environment.center, environment.randomOrientation()));
+`;
+
+export const herrErwin = `
+class Water extends Thing {
+    constructor(position) {
+        super(position);
+        
+        this.size = {x: 40, y: 40};
+        this.color = "aqua";
+    }
+    
+    interact(agent) {
+        agent.drink();
+    }
+}
+
+class Food extends Thing {
+    constructor(position) {
+        super(position);
+        
+        this.size = {x: 40, y: 40};
+        this.color = "green";
+    }
+    
+    interact(agent) {
+        agent.eat();
+    }
+}
+
+class HerrErwin extends Agent {
+    food = 100;
+    water = 100;
+    
+    constructor(position, orientation) {
+        super(position, orientation);
+
+        this.visual = "https://www.svgrepo.com/show/452885/donkey.svg";
+        
+        this.size = 50;
+    }
+    
+    levels(n) {
+        if (n <= 30)
+            return "P";
+        if (n >= 90)
+            return "M";
+        return "KP";
+    }
+    
+    eat() {
+        this.food += 2;
+        
+        if (this.food > 100) {
+            this.food = 100;
+        }
+    }
+    
+    drink() {
+        this.water += 2;
+        
+        if (this.water > 100) {
+            this.water = 100;
+        }
+    }
+
+    tick() {
+        this.right(10);
+        this.go(20);
+        
+        this.food--;
+        this.water--;
+        
+        if (this.food <= 0 || this.water <= 0) {
+            this.die();
+        }
+    }
+}
+
+environment.add(new HerrErwin(environment.center));
+
+environment.add(new Water({
+    x: environment.center.x + 300,
+    y: environment.center.y
+}));
+
+environment.add(new Food({
+    x: environment.center.x - 300,
+    y: environment.center.y
+}));
 `;
