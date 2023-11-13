413364a3189d5a00afc47d7475e2b761bd79d863
Alp Kaan Aksu <hi@alpkaanaksu.com>
2023-11-13 18:31:01 +0100

    Create ThingDisplay

---
+++ src/lib/ThingDisplay.svelte
@@ -0,0 +1,23 @@
+<script>
+	export let thing;
+</script>
+
+{#if thing.visual}
+	<image
+		href={thing.visual}
+		x={thing.position.x}
+		y={thing.position.y}
+		width={thing.size.x}
+		height={thing.size.y}
+        style="color: {thing.color};"
+	/>
+{:else}
+	<rect
+		x={thing.position.x}
+		y={thing.position.y}
+		width={thing.size.x}
+		height={thing.size.y}
+        fill={thing.color}
+        rx="8"
+	/>
+{/if}
