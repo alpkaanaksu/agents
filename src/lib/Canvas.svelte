a48d0512026e4a69e9fb98ba879b57370c07e825
Alp Kaan Aksu <hi@alpkaanaksu.com>
2023-11-13 22:54:29 +0100

    Add information display

--- src/lib/Canvas.svelte
+++ src/lib/Canvas.svelte
@@ -4,23 +4,56 @@
 	import ThingDisplay from './ThingDisplay.svelte';
 
 	export let environment;
+
+	let currentObject = {};
 </script>
 
-<svg id="environment" width={environment.dimensions.x} height={environment.dimensions.y}>
-	{#each environment.agents as agent}
-		{#if agent.alive}
-			<AgentDisplay {agent} />
-		{/if}
-	{/each}
+<div class="simulation">
+	<svg id="environment" width={environment.dimensions.x} height={environment.dimensions.y}>
+		{#each environment.agents as agent}
+			<g on:click={() => (currentObject = agent)}>
+				{#if agent.alive}
+					<AgentDisplay {agent} />
+				{/if}
+			</g>
+		{/each}
 
-	{#each environment.things as thing}
-		<ThingDisplay {thing} />
-	{/each}
-</svg>
+		{#each environment.things as thing}
+			<g on:click={() => (currentObject = thing)}>
+				<ThingDisplay {thing} />
+			</g>
+		{/each}
+	</svg>
+	{#if currentObject}
+		<div class="info">
+			{#each Object.entries(currentObject) as entry}
+				<div><b>{entry[0]}:</b> {JSON.stringify(entry[1])}</div>
+			{/each}
+		</div>
+	{/if}
+</div>
 
 <style>
 	#environment {
 		border-radius: 8px;
 		box-shadow: 0 0 8px #ccc;
+		box-sizing: border-box;
 	}
+
+	.simulation {
+		position: relative;
+	}
+
+	.info {
+		position: absolute;
+		bottom: 0;
+		left: 0;
+		border-radius: 8px;
+		padding: 8px 16px;
+	}
+
+	.info div {
+		margin-bottom: 8px;
+	}
+
 </style>
