4f2ace30d80d688f0151a7aecaf37f4b4537f6fb
Alp Kaan Aksu <hi@alpkaanaksu.com>
2023-11-15 23:08:45 +0100

    Create models/all.js

---
+++ models/all.js
@@ -0,0 +1,46 @@
+exposed.turn = 10;
+exposed.step = 5;
+
+// Set seed for reproducibility
+// Avoid when doing multiple runs!
+utils.setSeed("ABM")
+
+class Food extends Thing {
+    nutritionalValue = 1;
+    size = { x: 10, y: 10 }
+    color = "green"
+}
+
+class SuperFood extends Food {
+    nutritionalValue = 10;
+    color = "orangered"
+}
+
+class Sheep extends Agent {
+    foodReservoir = utils.random(1, 100);
+    waterReservoir = utils.random(1, 100);
+    size = 40;
+    visual = "https://www.svgrepo.com/show/24608/sheep.svg";
+
+    tick() {
+        this.right(exposed.turn);
+        this.go(exposed.step);
+        
+        this.foodReservoir -= 0.5;
+        this.waterReservoir -= 0.5;
+        
+        if (this.foodReservoir <= 0 || this.waterReservoir <= 0) {
+            this.die();
+        }
+    }
+}
+
+
+env.add(10, Sheep);
+
+env.onTick(() => {
+    if(utils.p(5)) {
+        environment.add(5, Food);
+        environment.add(1, SuperFood);
+    }
+});
